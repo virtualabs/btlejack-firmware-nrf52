82bf45c5c00a4f023ed815a4290a96132774095d
Damien Cauquil <virtualabs@gmail.com>
2022-05-18 01:19:50 +0200

    Fixed some bugs in timer implementation.

--- source/timer.cpp
+++ source/timer.cpp
@@ -1,6 +1,14 @@
 #include "timer.h"
 #include "link.h"
 
+/**
+ * @brief Constructor
+ * 
+ * Instanciate a CustomTimer:
+ * - associate a specific channel to this CustomTimer instance
+ * - relies on a single NRFLowLevelTimer able to handle 3 dedicated channels.
+ **/
+
 CustomTimer::CustomTimer() {
   /* Instanciate NRFLowLevelTimer if required. */
   if (CustomTimer::m_timer == NULL)
@@ -19,6 +27,13 @@ CustomTimer::CustomTimer() {
   CustomTimer::m_current_channel++;
 }
 
+/**
+ * @brief   Trigger a specific callback after a given period of time (in microseconds).
+ *          Callback is only called once.
+ * @param pfn_callback Callback function to be called.
+ * @param period Delay in microseconds.
+ **/
+
 void CustomTimer::attach_us(FCallback pfn_callback, uint32_t period)
 {
   /* Save period. */
@@ -29,17 +44,44 @@ void CustomTimer::attach_us(FCallback pfn_callback, uint32_t period)
 
   /* Save period. */
   CustomTimer::m_timer->setCompare(m_channel, CustomTimer::m_timer->captureCounter() + m_periods[m_channel]);
+  //CustomTimer::m_timer->offsetCompare(m_channel, /*CustomTimer::m_timer->captureCounter() + */m_periods[m_channel]);
 }
+
+
+/**
+ * @brief Get associated channel number.
+ * @return channel number (0-2)
+ **/
 
 int CustomTimer::get_channel(void)
 {
   return m_channel;
 }
 
+
+/**
+ * @brief Rearm this timer (callback will be called once its delay passed).
+ **/
+
+void CustomTimer::rearm(void)
+{
+  CustomTimer::m_timer->offsetCompare(m_channel, m_periods[m_channel]);
+}
+
+/**
+ * @brief Clear timer, it will not be triggered anymore.
+ **/
+
 void CustomTimer::detach(void)
 {
   CustomTimer::m_timer->clearCompare(m_channel);
 }
+
+/**
+ * @brief Internal callback used to dispatch timer interrupts.
+ * 
+ * @param channel_bitmsk Channel bitmask indicating triggered timer channels.
+ **/
 
 void CustomTimer::timer_callback(uint16_t channel_bitmsk)
 {
@@ -48,17 +90,20 @@ void CustomTimer::timer_callback(uint16_t channel_bitmsk)
   {
     if (channel_bitmsk & (1<<i))
     {
+      /* By default, clear timer. */
+      CustomTimer::m_timer->clearCompare(i);
+
       /* Trigger callback. */
       CustomTimer::m_callbacks[i]();
 
       /* Rearm timer. */
-      CustomTimer::m_timer->clearCompare(i);
-      CustomTimer::m_timer->offsetCompare(i, m_periods[i]);
+      //CustomTimer::m_timer->offsetCompare(i, m_periods[i]);
     }
   }
   CustomTimer::m_timer->enableIRQ();
 }
 
+/* Static values. */
 NRFLowLevelTimer *CustomTimer::m_timer = NULL;
 int CustomTimer::m_current_channel = 0;
 FCallback CustomTimer::m_callbacks[] = {NULL, NULL, NULL};
--- source/timer.h
+++ source/timer.h
@@ -1,3 +1,19 @@
+/**
+ * Micro:Bit Custom Timer
+ * 
+ * This module provides a CustomTimer class based on nRF52's NRF_TIMER0
+ * (Hardware timer).
+ * 
+ * When CustomTimer::attach_us() is called, it sets a callback function
+ * that will be triggered once the specified delay passed. 
+ * 
+ * /!\/!\/!\/!\
+ * 
+ * Be careful: this callback is in charge of rearming the timer if required.
+ * -------------------------------------------------------------------------
+ *  
+ **/
+
 #ifndef __INC_TIMER_H
 #define __INC_TIMER_H
 
@@ -5,6 +21,7 @@
 
 #define NB_CHANNELS_MAX 3
 
+/* Callback type. */
 typedef void (*FCallback)(void);
 
 /* uBit object from PXT or CODAL. 
@@ -15,24 +32,28 @@ typedef void (*FCallback)(void);
 extern MicroBit uBit;
 #endif
 
-
 class CustomTimer {
   
   private:
-    
-
     static void timer_callback(uint16_t channel_bitmsk);
-
     int m_channel;
 
+    static int m_current_channel;
+    static FCallback m_callbacks[NB_CHANNELS_MAX];
+    static uint32_t m_periods[NB_CHANNELS_MAX];
+    static NRFLowLevelTimer *m_timer;
+
   public:
-      int get_channel(void);
-      static int m_current_channel;
-      static FCallback m_callbacks[NB_CHANNELS_MAX];
-      static uint32_t m_periods[NB_CHANNELS_MAX];
-      static NRFLowLevelTimer *m_timer;
       CustomTimer();
+      int get_channel(void);
+            
+      /* Attach a callback to this timer that will be triggered after <period> microseconds. */
       void attach_us(FCallback pfn_callback, uint32_t period);
+
+      /* Rearm timer. */
+      void rearm(void);
+
+      /* Detach callback. */
       void detach(void);
 };
 
